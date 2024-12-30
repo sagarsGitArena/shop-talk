3d64f836e0b468d21a999f6bfd92390e3fb65a0d
Ubuntu <ubuntu@ip-172-31-83-8.ec2.internal>
2024-12-30 10:50:02 +0000

    testing setup script

--- set-up.sh
+++ set-up.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# Check if figlet is installed
 if ! dpkg -l | grep -q figlet; then
     sudo apt update
     sudo apt install -y figlet
@@ -6,23 +8,23 @@ else
     echo "Figlet is already installed."
 fi
 
-#printf 'Clone git repository' | figlet -w 200
-git clone git@github.com:sagarsGitArena/shop-talk.git
+# Clone the repository if it doesn't already exist
+if [ ! -d "/home/ubuntu/test/shop-talk" ]; then
+    printf "\e[31m$(figlet -w 200 "Cloning Git Repo")\e[0m\n"
+    git clone git@github.com:sagarsGitArena/shop-talk.git
+else
+    printf "\e[31m$(figlet -w 200 "Git Repo Already Cloned")\e[0m\n"
+fi
 
-printf "\e[31m$(figlet -w 200 Clone git repo )\e[0m"
-
-#curl -s https://raw.githubusercontent.com/sagarsGitArena/shop-talk/main/clone-git-repo.sh | bash
-
+# List the current directory's content
 pwd
-
 ls -lart 
 
-if [ -d “/home/ubuntu/test/shop-talk" ]; then
+# Check if the directory exists
+if [ -d "/home/ubuntu/test/shop-talk" ]; then
     echo "Directory exists"
-    cd /home/ubuntu/test/short-talk
+    cd /home/ubuntu/test/shop-talk || exit 1
     sh set-up-env.sh
 else
     echo "Directory does not exist"
 fi
-
-

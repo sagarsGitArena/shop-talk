d587e0e1559b3fbd1fe9a4bb404ab8ab81504f89
Saagar <vasamsetty@gmail.com>
2025-01-18 08:35:06 -0500

    fix setup file location

--- set-up/set-up.sh
+++ set-up/set-up.sh
@@ -35,9 +35,8 @@ ls -lart
 if [ -d "$REPO_DIR" ]; then
     echo "Directory exists: $REPO_DIR"
     cd "$REPO_DIR" || exit 1
-    sh set-up/set-up-env.sh
     git pull
-    sh set-up-env.sh
+    sh set-up/set-up-env.sh
 else
     echo "Directory does not exist: $REPO_DIR"
 fi

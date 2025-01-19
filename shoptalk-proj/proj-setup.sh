330e47f196cfa1c22d14227d755ddd1396aa2530
Ubuntu <ubuntu@ip-172-31-39-180.ec2.internal>
2025-01-19 01:14:21 +0000

    minor fix

--- shoptalk-proj/proj-setup.sh
+++ shoptalk-proj/proj-setup.sh
@@ -1,7 +1,11 @@
 
-./clean-dirs.sh
 
 mkdir -p ./dags
 cp -r ./airflow-dag/* ./dags
 mkdir -p ./logs ./plugins ./config ./data/raw ./data/rawimages
 echo -e "AIRFLOW_UID=$(id -u)" > .env
+
+echo -e "AIRFLOW_GID=0" >> .env
+echo -e "REGION_NAME=us-east-1" >> .env
+echo -e "AWS_ACCESS_KEY_ID='AKIA5LIHLU6G4CGEIUXM'" >> .env
+echo -e "AWS_SECRET_ACCESS_KEY='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'" >> .env

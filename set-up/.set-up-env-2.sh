9ac9e0d406aa7dca353a938a3d504cab3e7211da
Ubuntu <ubuntu@ip-172-31-77-91.ec2.internal>
2025-03-02 22:41:01 +0000

    minor corrections

--- set-up/.set-up-env-2.sh
+++ set-up/.set-up-env-2.sh
@@ -20,7 +20,7 @@ fi
 #printf "\e[32m$(echo -e 'Install NVIDIA-DOCKER' | figlet -w 300 -f slant)\e[0m"
 #echo ""
 
-figlet_printf-green 'NVIDIA  Docker'
+figlet_printf_green 'NVIDIA  Docker'
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID) && \
 curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - && \
 curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
